d1950031b8d34f04cee22d2660ff3b7d51e6aceb
Balieiro004 <balieiro004@gmail.com>
2023-06-01 11:23:40 -0300

    Adicionando internacionalização no arquivo HelloControlle

--- src/main/java/com/balieiro/rest/webservices/restfulwebservices/controller/HelloController.java
+++ src/main/java/com/balieiro/rest/webservices/restfulwebservices/controller/HelloController.java
@@ -1,16 +1,26 @@
 package com.balieiro.rest.webservices.restfulwebservices.controller;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloController {
 
+    private MessageSource messageSource;
+
+    public HelloController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "/hello-world")
     public String heloWorld(){
-        return "Hello Wold vamos ver";
+        return "Hello Wold";
     }
 
     @GetMapping(path = "/hello-world-bean")
@@ -21,5 +31,13 @@ public class HelloController {
     @GetMapping(path = "/hello-world/-variablepath/{name}")
     public HellotWorldBean heloWorldPathVariable(@PathVariable String name){
         return new HellotWorldBean("Hello Wold " + name);
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String heloWorldInternationalized(){
+
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+        //return "Hello Wold v2";
     }
 }
